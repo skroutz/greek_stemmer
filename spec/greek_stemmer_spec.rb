546cf8937053fb011ccd4176007f1823b8fd7d03
Christos Mantas <cmantas@skroutz.gr>
2017-02-20 16:11:11 +0200

    Add test for protected words
    Add a simple test that confirms that protected words are not stemmed.

--- spec/greek_stemmer_spec.rb
+++ spec/greek_stemmer_spec.rb
@@ -5,6 +5,8 @@ require "yaml"
 describe GreekStemmer do
   describe "#stem" do
     let(:words) { YAML.load_file('spec/fixtures/examples.yml') }
+    let(:config) { YAML.load_file('config/stemmer.yml') }
+
     it "stems words correctly" do
       words.each_pair do |word, stem|
         expect(described_class.stem(word)).to eq(stem)
@@ -14,6 +16,14 @@ describe GreekStemmer do
     context "when a non-greek word is used" do
       it "does not stem it" do
         expect(described_class.stem("englishΟΣ")).to eq("englishΟΣ")
+      end
+    end
+
+    context 'exceptions' do
+      it 'does not stem protected words' do
+        config['protected_words'].each do |word|
+          expect(described_class.stem(word)).to eq(word)
+        end
       end
     end
   end
